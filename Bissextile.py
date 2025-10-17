5e323a0933162325ffbed1cb5f14330e456bee90
MatarCoume03 <matarcoume03@gmail.com>
2025-10-17 13:16:12 +0200

    premiere optimisation du code

--- Bissextile.py
+++ Bissextile.py
@@ -1,14 +1,9 @@
+#Programme verifiant si une annee entree au clavier est bissextile ou non
 annee = input("Entrer l\'annee dont vous souhaitez verifier si elle est bissextile ou non: ")
 annee = int(annee)
 bissextile = False
-if annee % 4 == 0:
+if annee % 400 == 0 or (annee % 4 == 0 and annee % 100 != 0):
     bissextile = True
-elif annee % 100 == 0:
-    bissextile = False
-elif annee % 400 == 0:
-    bissextile = True
-else:
-    bissextile = False
 if bissextile:
     print("L'annee que vous avez saisie est bel et bien bissextile")
 else:
