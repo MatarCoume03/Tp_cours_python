d650c4f7fc88896078739fc4dc2f4e0559e443aa
MatarCoume03 <matarcoume03@gmail.com>
2025-10-18 18:15:59 +0200

    Optimisation avec la structure de controle match

--- Structures de controle/CalculatriceSimple.py
+++ Structures de controle/CalculatriceSimple.py
@@ -7,23 +7,24 @@ number2 = input("Saisissez le second nombre: ")
 number2 = float(number2)
 operation = input("Veuillez entrer le symbole de l'operation que vous souhaitez executer: ")
 
-if operation == "+":
-    resultat = number1 + number2
-    print("Vous avez choisi l'addition et voici le resultat: \n",number1 ," + ",number2, " = ", resultat)
-elif operation == "-":
-    resultat = number1 - number2
-    print("Vous avez choisi la soustraction et voici le resultat: \n",number1," - ",number2," = ",resultat)
-elif operation == "*":
-    resultat = number1 * number2
-    print("Vous avez choisi la multiplication et voici le resultat: \n",number1," * ",number2," = ",resultat)
-elif operation == "/":
-    if number2 == 0:
-        print("Le denominateur doit imperativement etre different de 0!")
-    else:
-        resultat = number1 / number2
-        print("Vous avez choisi la division et voici le resultat: \n",number1," / ",number2," = ",resultat)
-elif operation == "%":
-    resultat = number1 % number2
-    print("Vous avez choisi l'operation modulo et voici le resultat: \n",number1," % ",number2," = ",resultat)
-else:
-    print("Choix non disponible")
+match operation:
+    case "+":
+        resultat = number1 + number2
+        print("Vous avez choisi l'addition et voici le resultat: \n",number1 ," + ",number2, " = ", resultat)
+    case "-":
+        resultat = number1 - number2
+        print("Vous avez choisi la soustraction et voici le resultat: \n",number1," - ",number2," = ",resultat)
+    case "*":
+        resultat = number1 * number2
+        print("Vous avez choisi la multiplication et voici le resultat: \n",number1," * ",number2," = ",resultat)
+    case "/":
+        if number2 == 0:
+            print("Le denominateur doit imperativement etre different de 0!")
+        else:
+            resultat = number1 / number2
+            print("Vous avez choisi la division et voici le resultat: \n",number1," / ",number2," = ",resultat)
+    case "%":
+        resultat = number1 % number2
+        print("Vous avez choisi l'operation modulo et voici le resultat: \n",number1," % ",number2," = ",resultat)
+    case _:
+        print("Choix non disponible")
