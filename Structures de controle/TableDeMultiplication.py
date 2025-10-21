c7a9b391a99fd512bc01f45e11f65fb23d1e86f9
MatarCoume03 <matarcoume03@gmail.com>
2025-10-21 14:45:29 +0200

    amelioration avec fonction a parametre

--- Structures de controle/TableDeMultiplication.py
+++ Structures de controle/TableDeMultiplication.py
@@ -1,12 +1,10 @@
 # Affichez la table de multiplication d'un nombre donné
 # de 1 à 10, en utilisant une boucle for
 
-def table_de_multiplication():
-    nb = input("Saisissez un nombre pour qu'on vous donne sa table de mutiplication: ")
-    nb = int(nb)
+def table_de_multiplication(nb):
     i = 0
     while i < 10:
         print(nb, " * ", (i + 1), " = ", (i + 1) * nb)
         i += 1
 if __name__ == "__main__":
-    table_de_multiplication()
+    table_de_multiplication(3)
