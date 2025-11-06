0ea1ddc6700042168ab1c3480c7ff7be734dfaaa
MatarCoume03 <matarcoume03@gmail.com>
2025-11-06 09:28:40 +0100

    Amelioration du programme avec les exceptions

--- Bissextile.py
+++ Bissextile.py
@@ -1,7 +1,18 @@
 #Programme verifiant si une annee entree au clavier est bissextile ou non
-annee = input("Entrer l\'annee dont vous souhaitez verifier si elle est bissextile ou non: ")
-annee = int(annee)
+from os import system #importation de la fonction system du module os
+
+try:
+    annee = input("Entrer l\'annee dont vous souhaitez verifier si elle est bissextile ou non: ")
+    annee = int(annee)
+    assert annee > 0
+except ValueError:
+    print("Vous n'avez pas saisi de valeur!")
+except AssertionError:
+    print("L'année saisie est inférieure ou égale à 0.")
+    
 if annee % 400 == 0 or (annee % 4 == 0 and annee % 100 != 0):
-    print("L'annee que vous avez saisie est bel et bien bissextile")
+    print("L'année que vous avez saisie est bel et bien bissextile")
 else:
     print("L'annee que vous avez saisie n'est pas bissextile")
+
+system("pause") #cette fonction empeche votre programme de se refermer 
