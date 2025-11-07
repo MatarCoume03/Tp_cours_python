7d076b8ea8b222f4d7d22fd499da6d90b8cba5b1
MatarCoume03 <matarcoume03@gmail.com>
2025-11-07 11:39:53 +0100

    Gestion des erreurs de code

--- ZCasino.py
+++ ZCasino.py
@@ -10,36 +10,39 @@ def ZCasino():
     print("******************************************************************\n")
 
     #jeu de la roulette
-    print("1) Saisi du nombre")
-    try:
-        nb = int(input("Nombre: "))
-        assert 0 <= nb <= 49
-    except AssertionError:
-        print("Le nombre saisi doit etre compris entre 0 et 49")
+    while True:
+        print("1) Saisi du nombre")
+        try:
+            nb = int(input("Nombre: "))
+            assert 0 <= nb <= 49
+        except AssertionError:
+            print("Le nombre saisi doit etre compris entre 0 et 49")
+            continue
 
-    print("2) Saisi de la somme misee")
-    try:
-        sm = int(input("Somme: "))
-        assert sm > 0
-    except AssertionError:
-        print("LA Somme saisie doit etre strictement positif")
+        print("2) Saisi de la somme misee")
+        try:
+            sm = int(input("Somme: "))
+            assert sm > 0
+        except AssertionError:
+            print("LA Somme saisie doit etre strictement positif")
+            continue
 
-    #resume des choix du client
-    print(f"\nNombre Choisi: {nb}")
-    print(f"Somme misee: {sm}")
+        #resume des choix du client
+        print(f"\nNombre Choisi: {nb}")
+        print(f"Somme misee: {sm}")
 
-    #mise en marche de la roulette
-    hasar = random.randrange(0, 49)
-    print(f"Nombre sorti par la roulette: {hasar}")
-    if nb == hasar:
-        print("Felicitation vous etes l'heureux gagnant et vous remportez 3X la somme misee :)")
-        print(f"Somme gagnee = {3*sm}")
-    else:
-        if (nb % 2 == hasar % 2 == 0) or (nb % 2 != 0 and hasar % 2 != 0):
-            print("Ca va vous remporter la moitie de la somme misee comme recompense :|")
-            print(f"Somme gagnee: {math.ceil(sm + (sm / 2))}")
+        #mise en marche de la roulette
+        hasar = random.randrange(0, 49)
+        print(f"Nombre sorti par la roulette: {hasar}")
+        if nb == hasar:
+            print("Felicitation vous etes l'heureux gagnant et vous remportez 3X la somme misee :)")
+            print(f"Somme gagnee = {3*sm}")
         else:
-            print("Desole une prochaine fois peut etre :(")
+            if (nb % 2 == hasar % 2 == 0) or (nb % 2 != 0 and hasar % 2 != 0):
+                print("Ca va vous remporter la moitie de la somme misee comme recompense :|")
+                print(f"Somme gagnee: {math.ceil(sm + (sm / 2))}")
+            else:
+                print("Desole une prochaine fois peut etre :(")
 
 
 if __name__ == "__main__":
