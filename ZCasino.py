8881bedb017332ee93430345786953fb04518cd8
MatarCoume03 <matarcoume03@gmail.com>
2025-11-09 00:49:59 +0100

    3eme optimisation

--- ZCasino.py
+++ ZCasino.py
@@ -24,7 +24,7 @@ def ZCasino():
         print("2) Saisi de la somme misee")
         try:
             sm = int(input("Somme: "))
-            assert argent > sm
+            assert argent >= sm
         except AssertionError:
             print("Desole la somme que vous chercher a misee est superieur a l'argent qui vous reste :(")
             continue
@@ -40,15 +40,18 @@ def ZCasino():
             print("Felicitation vous etes l'heureux gagnant et vous remportez 3X la somme misee :)")
             print(f"Somme gagnee = {sm * 3}")
             argent += sm * 3
+            print(f"Desormais vous avez {argent} en poche")
         else:
             if (nb % 2 == hasar % 2 == 0) or (nb % 2 != 0 and hasar % 2 != 0):
                 print("Ca va vous remporter la moitie de la somme misee comme recompense :|")
                 sm = math.ceil(sm / 2)
                 print(f"Somme gagnee: {sm}")
                 argent += sm
+                print(f"Desormais vous avez {argent} en poche")
             else:
                 print("Desole une prochaine fois peut etre :(")
                 argent -= sm
+                print(f"Desormais vous avez {argent} en poche")
 
         continuer = input("Voulez vous faire une autre partie?")
         if continuer.lower() != 'o':
