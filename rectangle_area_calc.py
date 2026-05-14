1957d7896d24e31c174739d1d6fac147f7752608
MatarCoume03 <matarcoume03@gmail.com>
2026-05-14 13:08:08 +0200

    Comment calculer l'aire d'un rectangle

---
+++ rectangle_area_calc.py
@@ -0,0 +1,8 @@
+#Exercise 1 Rectangle Area Calc
+
+print("Veuillez rentrer les dimensions du rectangle pour calculer son aire.")
+longueur = float(input("Longueur du rectangle: "))
+largeur = float(input("Largeur du rectangle: "))
+print("***petit rappel: Aire = longueur x largeur ***")
+aire = longueur * largeur
+print(f"L'aire du rectangle est de: {longueur} x {largeur} = {aire} m^2")
