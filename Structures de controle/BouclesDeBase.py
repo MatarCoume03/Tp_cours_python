00c133ecb003fbc5da6addaf80bc935c33174a18
MatarCoume03 <matarcoume03@gmail.com>
2026-09-25 17:47:40 +0200

    Affichage 1 des elements de la liste avec la boucle for

---
+++ Structures de controle/BouclesDeBase.py
@@ -0,0 +1,9 @@
+"""
+Boucles de base
+Soit la liste ["vache", "souris", "levure", "bacterie"]. Affichez l’ensemble des éléments de cette liste (un
+élément par ligne) de trois façons différentes (deux méthodes avec for et une avec while).
+"""
+#Affichage 1 avec la boucle for:
+liste = ["Vache", "Souris", "Levure", "Bacterie"]
+for element in liste:
+    print(element)
