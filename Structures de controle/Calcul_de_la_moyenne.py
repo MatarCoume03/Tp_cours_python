cb0b1e9f0bcd838bcae19462ec6f2c16dd2d32c5
MatarCoume03 <matarcoume03@gmail.com>
2026-09-26 13:55:02 +0200

    Calcul de la moyenne d'un etudiant

---
+++ Structures de controle/Calcul_de_la_moyenne.py
@@ -0,0 +1,7 @@
+"""
+Calcul de la moyenne
+Voici les notes d’un étudiant [14, 9, 6, 8, 12]. Calculez la moyenne de ces notes. Utilisez l’écriture formatée
+pour afficher la valeur de la moyenne avec deux décimales.
+"""
+notes = [14, 9, 6, 8, 12]
+print(f"La moyenne de l'etudiant est de: {(sum(notes)/len(notes)):.2f}")
